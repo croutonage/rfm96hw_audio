0e480f2250cd8d43db414e9ba7438e64c3d51dae
Bertrik Sikken <bertrik@sikken.nl>
2014-08-02 18:00:28 +0200

    Implement audio playback from flash memory

--- rfm96hw_audio.ino
+++ rfm96hw_audio.ino
@@ -3,11 +3,11 @@
 #include "lplRFM69registersh.h"
 #include "lplRFM69h.h"
 
+#include "waakhond.h"
+
 RFM69 radio(SPI_CS, RF69_IRQ_PIN, true);
 
-#define SAMPLE_FREQ  10000L
-
-static int tabel[16];
+#define SAMPLE_FREQ  8000L
 
 void setup() {
   Serial.begin(115200);
@@ -19,26 +19,31 @@ void setup() {
 
   radio.setMode(RF69_MODE_TX);
   
-  int i;
-  for (i = 0; i < 16; i++) {
-    tabel[i] = 120 * (sin(i / 16.0 * 2 * PI) + 1.0);
+  // initialize timer1 
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  OCR1A = 8;                // compare match register 16MHz/256/x
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  interrupts();             // enable all interrupts  
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  static int addr = 44;
+  uint8_t b = pgm_read_byte_near(waakhond_wav + addr);
+  radio.writeReg(0x09, b);
+  
+  addr++;
+  if (addr >= waakhond_wav_len) {
+    addr = 44;
   }
 }
 
-int t = 0;
-double freq1 = 1000.0, freq2 = 500.0;
-
 void loop() {
-  int vi = tabel[t];
-
-  if (vi < 0)
-    vi = 0;
-  else if (vi > 255)
-    vi = 255;
-  
-  radio.writeReg(0x09, vi);
-  
-  t = (t + 1) % 16;
-  
-  delayMicroseconds(1);
+  // runs in interrupt, nothing to do here
 }
