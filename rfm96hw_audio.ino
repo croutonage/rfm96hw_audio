64c1c1256e9397742bc0125a2fb3ebadeadaac35
Bertrik Sikken <bertrik@sikken.nl>
2014-08-02 20:28:42 +0200

    Use smaller prescaler for more accurate sample rate

--- rfm96hw_audio.ino
+++ rfm96hw_audio.ino
@@ -25,9 +25,9 @@ void setup() {
   TCCR1B = 0;
   TCNT1  = 0;
 
-  OCR1A = 8;                // compare match register 16MHz/256/x
+  OCR1A = 250000/SAMPLE_FREQ;                // compare match register 16MHz/64/x
   TCCR1B |= (1 << WGM12);   // CTC mode
-  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TCCR1B |= (1 << CS10) | (1 << CS11);    // 64 prescaler 
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
   interrupts();             // enable all interrupts  
 }
